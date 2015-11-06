b123621df9e9a542c5696cd5ca33e1b7a4b9daaf
Oleg Mironov <allhakervbg@gmail.com>
2015-11-06 22:55:31 +0200

    routing changes

--- js/app.js
+++ js/app.js
@@ -1,11 +1,19 @@
 (function() {
-  var app = angular.module('typeYourName', ['ngRoute', 'ui.bootstrap', 'testController']);
+  var app = angular.module('typeYourName', ['ngRoute', 'ui.bootstrap', 'userController']);
 
   app.config(['$routeProvider', '$httpProvider', function($routeProvider, $httpProvider) {
     $routeProvider.
     when('/', {
-      templateUrl: "html/test.html",
+      templateUrl: "html/intro.html",
       controller: "mainController"
+    }).
+    when('/test', {
+        templateUrl: "html/test.html",
+        controller: "mainController"
+    }).
+    when('/login', {
+        templateUrl: "html/login.html",
+        controller: "loginController"
     });
   }]);
 
